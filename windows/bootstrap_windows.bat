aa87e09d80e065a8a0618c8e0508e8f6d22f1d21
dstamen <davidstamen@Davids-MacBook-Pro.local>
2015-08-28 13:27:16 -0400

    removed commands, not able to complete after reboot

--- windows/bootstrap_windows.bat
+++ windows/bootstrap_windows.bat
@@ -3,10 +3,3 @@ echo 'Install ADDS'
 
 echo 'Install ADDS Forest'
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "Install-ADDSForest -DomainName "lab.local" -DomainNetbiosName "lab" -SafeModeAdministratorPassword (ConvertTo-SecureString "P@ssword1" -AsPlainText -Force) -Verbose -Force"
-
-echo 'Create OUs'
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "Import-Module ActiveDirectory"
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "New-ADOrganizationalUnit "Lab Users""
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "New-ADOrganizationalUnit "Lab Groups""
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "New-ADOrganizationalUnit "Lab Servers""
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "New-ADOrganizationalUnit "Lab Desktops""
