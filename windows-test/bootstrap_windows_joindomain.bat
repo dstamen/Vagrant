f0839e3d32ad0dd8e88f50a994a8dec267115317
dstamen <davidstamen@Davids-MacBook-Pro.local>
2015-08-30 20:51:45 -0400

    removed windows-test

--- windows-test/bootstrap_windows_joindomain.bat
+++
@@ -1,1 +0,0 @@
-@powershell.exe;"$domain = 'lab.local';$password = 'vagrant' | ConvertTo-SecureString -asPlainText -Force;$username = '$domain\vagrant';$credential = New-Object System.Management.Automation.PSCredential($username,$password);Add-Computer -DomainName $domain -Credential $credential;Restart-Computer"
