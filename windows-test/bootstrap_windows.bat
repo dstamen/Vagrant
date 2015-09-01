99b799286084190abc8d8206145d11f11768e5c8
dstamen <davidstamen@Davids-MacBook-Pro.local>
2015-09-01 11:59:56 -0400

    .

--- windows-test/bootstrap_windows.bat
+++
@@ -1,1 +0,0 @@
-PowerShell -NoProfile -ExecutionPolicy Bypass -file "c:\vagrant\scripts\join_lab_domain.ps1"
--- windows-test/bootstrap_windows_dc_2_install.bat
+++
@@ -1,1 +0,0 @@
-PowerShell -NoProfile -ExecutionPolicy Bypass -file "c:\vagrant\scripts\install_lab_dc_2.ps1"
